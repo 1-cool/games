15b3fcb0e24f78dfd35d114ef67d8e80a786d998
1-cool <38838272+1-cool@users.noreply.github.com>
2018-10-07 21:21:07 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,105 @@
 #include<stdio.h>
+
 #include<stdlib.h>
+
+#include<conio.h>
+
 int move(int x,int y)
+
 {
-	for(int i=0;i<=y;i++)
-	{
+
+	for(int i=0;i<=y;i++)	{
+
 		printf("\n");
+
 	}
+
 	for(int i=0;i<x;i++)
+
 	{
+
 		printf(" ");
+
 	}
+
 	printf("   *\n");
+
 	for(int i=0;i<x;i++)
+
 	{
+
 		printf(" ");
+
 	}
+
 	printf(" *****\n");
 
 	for(int i=0;i<x;i++)
 
 	{
+
 		printf(" ");
+
 	}
+
 	printf("  * *\n");
+
 }
 
 int main()
+
 {
-	//while(1)
+
+	int x=25,y=6,input;
+
+	while(1)
+
 	{
-		move(20,6);
+
+		system("cls");
+
+		move(x,y);
+
+		if(kbhit())
+
+		{
+
+			input=getch();
+
+			if(input=='w')
+
+			{
+
+				y--;
+
+			}
+
+			if(input=='a')
+
+			{
+
+				x--;
+
+			}
+
+			if(input=='s')
+
+			{
+
+				y++;
+
+			}
+
+			if(input=='d')
+
+			{
+
+				x++;
+
+			}
+
+		}
+
 	}
+
 }
