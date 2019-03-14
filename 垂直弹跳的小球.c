eb58067498cdf0d38303f83f6e496e381c678a85
1-cool <38838272+1-cool@users.noreply.github.com>
2019-03-14 13:13:10 +0800

    Update 垂直弹跳的小球.c

--- 垂直弹跳的小球.c
+++ 垂直弹跳的小球.c
@@ -6,7 +6,7 @@ int main()
 	int x = 25, y = 6;			//小球的起始位置
 	int bottom = 20;			//地面的纵坐标，即小球运动的下界
 	int top = 6;				//球运动的上界
-	int v = 1;					//小球的速度
+	int v = 1;				//小球的速度
 	while (top <= bottom)
 	{
 		y += v;
@@ -15,7 +15,7 @@ int main()
 			printf("\n");
 		for (int i = 0; i < x; i++)
 			printf(" ");
-		printf("O\n");						//绘制小球
+		printf("O\n");					//绘制小球
 		if (y >= bottom)
 		{
 			top += 2;			//上界每次向下移动两行
