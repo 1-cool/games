d5b96bb229b969ceb569fa546a62dc7ed70ec20b
1-cool <38838272+1-cool@users.noreply.github.com>
2019-03-14 12:49:40 +0800

    Update 垂直弹跳的小球.c

--- 垂直弹跳的小球.c
+++ 垂直弹跳的小球.c
@@ -3,19 +3,19 @@
 #include<windows.h>
 int main()
 {
-	int x = 25, y = 5;			//小球的起始位置
+	int x = 25, y = 6;			//小球的起始位置
 	int	bottom = 20;			//地面的纵坐标，即小球运动的下界
-	int top = 4;				//球运动的上界
+	int top = 6;				//球运动的上界
 	int v = 1;					//小球的速度
 	while (top <= bottom)
 	{
 		y += v;
 		system("cls");
-		for (int i = 0; i < y; i++)
+		for (int i = 0; i < y; i++)			//绘制小球前的空行
 			printf("\n");
 		for (int i = 0; i < x; i++)
 			printf(" ");
-		printf("O\n");
+		printf("O\n");						//绘制小球
 		if (y >= bottom)
 		{
 			top += 2;			//上界每次向下移动两行
@@ -23,7 +23,7 @@ int main()
 		}
 		if (y <= top)
 			v = -v;
-		Sleep(5);
+		//Sleep(5);
 	}
 	return 0;
 }
