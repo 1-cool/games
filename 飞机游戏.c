3129deaec451af7b2d4ed41f570553e5dfd3db0f
1-cool <38838272+1-cool@users.noreply.github.com>
2019-03-15 14:50:17 +0800

    Update 飞机游戏.c

--- 飞机游戏.c
+++ 飞机游戏.c
@@ -2,75 +2,111 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<Windows.h>
-int x = 50, y = 20;			//飞机初始位置
-bool attack = false;			//储存开火状态
-void draw()				//绘制
+int x, y;				//飞机位置
+int bullet_x, buller_y;			//子弹位置
+int enemy_x, enemy_y;			//敌机位置
+int higt, widht;			//游戏画面尺寸
+int score;				//得分
+void HideCursor()				//隐藏光标
 {
-	if (!attack)						//没有攻击
-	{
-		for (int i = 0; i < y; i++)			//绘制飞机前的空行
-		{
-			printf("\n");
-		}
-	}
-	else							//进行攻击
-	{
-		for (int j = 0; j < y; ++j)			//绘制子弹
-		{
-			for (int i = 0; i < x; ++i)
-				printf(" ");
-			printf("|\n");
-		}
-		attack = false;
-		Beep(10000000, 2);
-	}
-	for (int i = 0; i < x; i++)				//绘制机头所在行前面的空格
-	{
-		printf(" ");
-	}
-	printf("*\n");						//绘制机头
-	for (int i = 0; i < x - 2; i++)				//绘制机翼
-	{
-		printf(" ");
-	}
-	printf("*****\n");
-	for (int i = 0; i < x - 1; i++)				//绘制机尾
-	{
-		printf(" ");
-	}
-	printf("* *\n");
+	CONSOLE_CURSOR_INFO cursor_info = { 1,0 };
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
-
-int main()
+void gotoxy(int x, int y)		//类似于清屏函数，光标移动到原点位置进行重画
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(handle, pos);
+}
+void startup()					//数据初始化
+{
+	higt = 20; widht = 30;		//设置画面尺寸
+	x = widht / 2; y = higt / 2;				//飞机初始位置
+	enemy_x = widht / 2; enemy_y = 0;	//敌机初始位置
+	score = 0;
+}
+void draw()						//绘制
+{
+	gotoxy(0, 0);
+	for (int i = 0; i < higt; ++i)
+	{
+		for (int j = 0; j < widht; ++j)
+		{
+			if (i == y && j == x)
+				printf("*");						//绘制飞机
+			else if (enemy_x == j && enemy_y == i)
+				printf("+");						//绘制敌机
+			else if (bullet_x == j && buller_y == i)
+				printf("|");						//绘制子弹
+			else
+				printf(" ");
+		}
+		printf("|\n");
+	}
+	for (int i = 0; i < widht; ++i)
+		printf("-");
+	printf("|\n得分：%d\n", score);
+}
+void update_without_input()
+{
+	if (buller_y >= 0)					//移动子弹
+		--buller_y;
+	if (buller_y == enemy_y && bullet_x == enemy_x)			//子弹击中敌机
+	{
+		++score;
+		Beep(10000, 2);
+		enemy_y = -1;					//产生新敌机
+		enemy_x = rand() % widht;
+		buller_y = -2;
+	}
+	if (enemy_y > higt)					//如果敌机跑出屏幕
+	{
+		enemy_y = -1;					//产生新敌机
+		enemy_x = rand() % widht;
+	}
+	/*用来控制敌机向下移动的速度。每隔几次循环，才移动一次敌机
+	这样修改的话，用户按键交互速度还是保持很快，但我们NPC的移动显示可以降速*/
+	static int speed = 0;
+	if (speed < 10)
+		++speed;
+	else
+	{
+		++enemy_y;
+		speed = 0;
+	}
+}
+void update_with_input()
 {
 	char input;
-	while (1)
+	if (_kbhit())				// 判断是否有输入,VS中不允许使用kbhit，这里使用_kbhit代替
 	{
-		system("cls");
-		draw();
-		if (_kbhit())				// 判断是否有输入,VS中不允许使用kbhit，这里使用_kbhit代替
+		input = _getch();		//VS中不允许使用getch，这里使用_getch代替
+		if (input == 'w' && y > 0)		//飞机上移
+			y--;
+		if (input == 'a' && x > 0)		//飞机左移
+			x--;
+		if (input == 's' && y < higt - 1)		//飞机下移
+			y++;
+		if (input == 'd' && x < widht - 1)		//飞机右移
+			x++;
+		if (input == ' ')		//飞机发射子弹
 		{
-			input = _getch();		//VS中不允许使用getch，这里使用_getch代替
-			if (input == 'w')		//飞机上移
-			{
-				y--;
-			}
-			if (input == 'a')		//飞机左移
-			{
-				x--;
-			}
-			if (input == 's')		//飞机下移
-			{
-				y++;
-			}
-			if (input == 'd')		//飞机右移
-			{
-				x++;
-			}
-			if (input == ' ')		//飞机发射子弹
-			{
-				attack = true;
-			}
+			bullet_x = x;
+			buller_y = y - 1;
 		}
 	}
+}
+int main()
+{
+	HideCursor();			//隐藏光标
+	startup();				//数据初始化
+	while (1)
+	{
+		draw();				//绘制
+		update_without_input();				//更新与用户无关的内容
+		update_with_input();				//更新与用户有关的内容
+	}
+	return 0;
 }
